fde13dcbe599f7e711d5150e2c8324d491602a38
ripandeep1750 <71696207+ripandeep1750@users.noreply.github.com>
2020-11-06 19:15:47 +0530

    Create case.r

---
+++ case.r
@@ -0,0 +1,12 @@
+# Changing to Upper case.
+result <- toupper("Changing To Upper")
+print(result)
+
+# Changing to lower case.
+result <- tolower("Changing To Lower")
+print(result)
+
+
+OUTPUT:-
+[1] "CHANGING TO UPPER"
+[1] "changing to lower"
