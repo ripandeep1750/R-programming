5ff496aa6499dc826d57dbaf4aa709a858b85010
ripandeep1750 <71696207+ripandeep1750@users.noreply.github.com>
2020-11-06 19:10:28 +0530

    Update paste_function.r

--- paste_function.r
+++ paste_function.r
@@ -4,3 +4,8 @@ c <- "are you? "
 print(paste(a,b,c))
 print(paste(a,b,c, sep = "-"))
 print(paste(a,b,c, sep = "", collapse = ""))
+
+OUTPUT:-
+[1] "Hello How are you? "
+[1] "Hello-How-are you? "
+[1] "HelloHoware you? "
